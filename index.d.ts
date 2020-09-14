6325053f87b52bf640ce3f5d6e891743bf02664a
begoat <hylbegoat@gmail.com>
2020-09-14 16:00:04 +0800

    feat(d.ts): make type definition more procise

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,37 @@
 import * as React from 'react';
 
-interface InteractionModalProps {
-  okButtonText?: string;
+/**
+ * okButtonText?: string;
   cancelButtonText?: string;
   showCancelButton?: boolean;
+ */
+
+interface AlertModalProps {
+  okButtonText?: string;
+}
+
+interface ConfirmModalProps {
+  okButtonText?: string;
+  cancelButtonText?: string;
+}
+
+interface PromoteModalProps {
+  okButtonText?: string;
+  cancelButtonText?: string;
 }
 
 export function alert(
   message?: React.ReactNode,
-  modalConfig?: InteractionModalProps
+  modalConfig?: AlertModalProps
 ): void;
 
 export function confirm(
   message?: React.ReactNode,
-  modalConfig?: InteractionModalProps
+  modalConfig?: ConfirmModalProps
 ): Promise<boolean>;
 
 export function prompt(
   message?: React.ReactNode,
   _default?: string,
-  modalConfig?: InteractionModalProps
+  modalConfig?: PromoteModalProps
 ): Promise<string | null>;
