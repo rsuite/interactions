a605e206c0aa067506bc66add61f1fdf88a0e509
Doma <leishenghao@126.com>
2020-09-18 18:08:55 +0800

    fix: Fix file name casing

---
+++ src/InteractionManager.js
@@ -0,0 +1,50 @@
+import alert from './alert';
+import confirm from './confirm';
+import prompt from './prompt';
+
+/**
+ * 用于队列 interaction 请求
+ */
+class InteractionManager {
+  $tail = null;
+
+  $interactionMap = {
+    alert,
+    confirm,
+    prompt,
+  };
+
+  getInteractionMethod(method) {
+    return this.$interactionMap[method];
+  }
+
+  interactionMethodExists(method) {
+    return !!this.$interactionMap[method];
+  }
+
+  requestInteraction(method, ...args) {
+    if (!this.interactionMethodExists(method)) {
+      return Promise.reject(
+        new Error(`No such interaction '${method}' found.`)
+      );
+    }
+    return this.performInteraction(method, ...args);
+  }
+
+  performInteraction(method, ...args) {
+    const tail = this.$tail;
+
+    this.$tail = new Promise(async resolve => {
+      try {
+        await tail;
+      } finally {
+        const interactionMethod = this.$interactionMap[method];
+        resolve(interactionMethod(...args));
+      }
+    });
+
+    return this.$tail;
+  }
+}
+
+export default new InteractionManager();
