045a72db3defc7b2f3e42b85398434748d5ab162
Doma <leishenghao@126.com>
2020-10-21 19:09:35 +0800

    test(unit): Add test for queueing interactions

--- __tests__/InteractionManager.js
+++ __tests__/InteractionManager.js
@@ -1,8 +1,31 @@
 import manager from '../src/InteractionManager';
+import { screen, fireEvent } from '@testing-library/react';
 
 it('should throw if invalid methods are requested', async () => {
   const dialog = jest.fn(
     async () => await manager.requestInteraction('dialog')
   );
   expect(dialog()).rejects.toThrow();
+});
+
+it('Queues multiple interaction requests', async () => {
+  manager.requestInteraction('alert', 'Message 1');
+  manager.requestInteraction('alert', 'Message 2');
+
+  const modal2 = screen.queryByText('Message 2', {
+    selector: '.rs-modal-body',
+  });
+  expect(modal2).not.toBeInTheDocument();
+
+  const okButton = (
+    await screen.findByText('Message 1', {
+      selector: '.rs-modal-body',
+    })
+  )
+    .closest('.rs-modal')
+    .querySelector('.rs-btn-primary');
+  fireEvent.click(okButton);
+  await screen.findByText('Message 2', {
+    selector: '.rs-modal-body',
+  });
 });
