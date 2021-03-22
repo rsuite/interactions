e83c476b3c49ff7cb79bb9598dd14b7ce4ae7861
sun <qiang.sun@mail.hypers.com>
2021-03-22 16:32:24 +0800

    update: 测试兼容rsuite@4/@5

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,27 @@
 import '@testing-library/jest-dom';
+
+if (process.env.RSUITE === '5') {
+  jest.mock(
+    'date-fns/_lib/toInteger/index.js',
+    () => {
+      return jest.requireActual('date-fns2/_lib/toInteger/index.js');
+    },
+    { virtual: true }
+  );
+
+  jest.mock(
+    'date-fns/_lib/cloneObject',
+    () => {
+      return jest.requireActual('date-fns2/_lib/cloneObject');
+    },
+    { virtual: true }
+  );
+
+  jest.mock('rsuite', () => {
+    return jest.requireActual('rsuite5');
+  });
+} else {
+  jest.mock('rsuite', () => {
+    return jest.requireActual('rsuite4');
+  });
+}
