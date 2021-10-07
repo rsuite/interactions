62f1a4897ec23d159182784f74b96bc5363b32f9
Doma <leishenghao@126.com>
2021-10-07 22:49:09 +0800

    test: refactor test cases

---
+++ src/__tests__/prompt.test.js
@@ -0,0 +1,249 @@
+import {
+  screen,
+  waitForElementToBeRemoved,
+  fireEvent,
+  within,
+  act,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import manager from '../InteractionManager';
+import prompt from '../prompt';
+
+afterEach(async () => {
+  manager.resetQueue();
+});
+
+it('shows dialog with given message and an text input and two buttons', async () => {
+  const message = 'Message';
+  prompt(message);
+
+  const dialog = screen.getByRole('dialog');
+  expect(dialog).toBeVisible();
+  expect(dialog).toHaveTextContent(message);
+
+  expect(within(dialog).getByRole('textbox')).toBeInTheDocument();
+  expect(
+    within(dialog).getByRole('button', { name: '取消' })
+  ).toBeInTheDocument();
+  expect(
+    within(dialog).getByRole('button', { name: '确定' })
+  ).toBeInTheDocument();
+});
+
+it('renders default value', async () => {
+  const defaultValue = 'Default value';
+  prompt('Message', defaultValue);
+
+  expect(screen.getByRole('textbox')).toHaveValue(defaultValue);
+});
+
+it('renders custom button text', async () => {
+  const okButtonText = 'Okay';
+  const cancelButtonText = 'Nah';
+  prompt('Message', '', {
+    okButtonText,
+    cancelButtonText,
+  });
+
+  expect(
+    screen.getByRole('button', { name: cancelButtonText })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole('button', { name: okButtonText })
+  ).toBeInTheDocument();
+});
+
+describe('resolves correctly', () => {
+  const inputValue = 'Input value';
+
+  it('hides dialog and resolves input value on clicking ok button', async () => {
+    const promise = prompt('Message');
+    userEvent.type(screen.getByRole('textbox'), inputValue);
+    userEvent.click(screen.getByRole('button', { name: '确定' }));
+    await expect(promise).resolves.toBe(inputValue);
+  });
+
+  it('hides dialog and resolves null on clicking cancel button', async () => {
+    const promise = prompt('Message');
+    userEvent.type(screen.getByRole('textbox'), inputValue);
+    userEvent.click(screen.getByRole('button', { name: '取消' }));
+    await expect(promise).resolves.toBe(null);
+  });
+
+  it('hides dialog and resolves input value on pressing Enter', async () => {
+    const promise = prompt('Message');
+    userEvent.type(screen.getByRole('textbox'), inputValue);
+    fireEvent.keyDown(document, { key: 'Enter' });
+    await expect(promise).resolves.toBe(inputValue);
+  });
+
+  it('hides dialog and resolves null on pressing Esc', async () => {
+    const promise = prompt('Message');
+    userEvent.type(screen.getByRole('textbox'), inputValue);
+    fireEvent.keyDown(document, { key: 'Escape' });
+    await expect(promise).resolves.toBe(null);
+  });
+});
+
+describe('triggers callbacks', () => {
+  it('calls onOk on clicking ok button', async () => {
+    const onOk = jest.fn();
+    prompt('Message', '', {
+      onOk,
+    });
+
+    userEvent.click(screen.getByRole('button', { name: '确定' }));
+    expect(onOk).toHaveBeenCalled();
+  });
+
+  it('calls onCancel on clicking cancel button', async () => {
+    const onCancel = jest.fn();
+    prompt('Message', '', {
+      onCancel,
+    });
+
+    userEvent.click(screen.getByRole('button', { name: '取消' }));
+    expect(onCancel).toHaveBeenCalled();
+  });
+
+  describe('waits for async onOk', () => {
+    it('shows loading on ok button', async () => {
+      const asyncOnOk = jest.fn(
+        () =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 1000);
+          })
+      );
+      const promise = prompt('Message', '', {
+        onOk: asyncOnOk,
+      });
+
+      const okButton = screen.getByRole('button', { name: '确定' });
+      userEvent.click(okButton);
+      expect(okButton).toHaveClass('rs-btn-loading');
+
+      await act(() => promise);
+    });
+
+    it('disables cancel button while promise running', async () => {
+      const asyncOnOk = jest.fn(
+        () =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 1000);
+          })
+      );
+      const promise = prompt('Message', '', {
+        onOk: asyncOnOk,
+      });
+
+      const okButton = screen.getByRole('button', { name: '确定' });
+      userEvent.click(okButton);
+
+      expect(screen.getByRole('button', { name: '取消' })).toBeDisabled();
+      await act(() => promise);
+    });
+
+    it("doesn't disable  cancel button if onCancel is provided", async () => {
+      const asyncOnOk = jest.fn(
+        () =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 1000);
+          })
+      );
+      const promise = prompt('Message', '', {
+        onOk: asyncOnOk,
+        onCancel: jest.fn(),
+      });
+
+      const okButton = screen.getByRole('button', { name: '确定' });
+      userEvent.click(okButton);
+
+      expect(screen.getByRole('button', { name: '取消' })).not.toBeDisabled();
+      await act(() => promise);
+    });
+
+    it("doesn't disable cancel button if canCancelOnLoading is true", async () => {
+      const asyncOnOk = jest.fn(
+        () =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 1000);
+          })
+      );
+      const promise = prompt('Message', '', {
+        onOk: asyncOnOk,
+        canCancelOnLoading: true,
+      });
+
+      const okButton = screen.getByRole('button', { name: '确定' });
+      userEvent.click(okButton);
+
+      expect(screen.getByRole('button', { name: '取消' })).not.toBeDisabled();
+      await act(() => promise);
+    });
+
+    it('force disable cancel button if canCancelOnLoading is false', async () => {
+      const asyncOnOk = jest.fn(
+        () =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 1000);
+          })
+      );
+      const promise = prompt('Message', '', {
+        onOk: asyncOnOk,
+        onCancel: jest.fn(),
+        canCancelOnLoading: false,
+      });
+
+      const okButton = screen.getByRole('button', { name: '确定' });
+      userEvent.click(okButton);
+
+      expect(screen.getByRole('button', { name: '取消' })).toBeDisabled();
+      await act(() => promise);
+    });
+
+    it('closes after promise finishes', async () => {
+      jest.useFakeTimers();
+      const asyncOnOk = jest.fn(
+        () =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 1000);
+          })
+      );
+      prompt('Message', '', {
+        onOk: asyncOnOk,
+      });
+
+      const okButton = screen.getByRole('button', { name: '确定' });
+      userEvent.click(okButton);
+
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+      await waitForElementToBeRemoved(screen.getByRole('dialog'));
+      jest.useRealTimers();
+    });
+  });
+});
+
+describe('resolve input props in dialog config', () => {
+  it('input should be disabled', async () => {
+    prompt('Message', '', {
+      inputProps: {
+        disabled: true,
+      },
+    });
+    expect(screen.getByRole('textbox')).toBeDisabled();
+  });
+});
